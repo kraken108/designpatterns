04eba2f815c51947ddd9afac9691043cc11dfe13
dagoldenburg <dagoldenburg@gmail.com>
2018-03-12 18:27:44 +0100

    Merge branch 'master' of https://github.com/kraken108/designpatterns
    # Conflicts:
    #	src/Model/DrawApplication.java

--- src/Model/Commands/EditDrawCommand.java
+++ src/Model/Commands/EditDrawCommand.java
@@ -1,4 +1,49 @@
 package Model.Commands;
 
-public class EditDrawCommand {
+import Model.Shapes.Shape;
+import Model.Shapes.ShapeFactory;
+
+public class EditDrawCommand extends Command {
+    private Shape prevShape;
+    private Shape newShape;
+    private int index;
+
+    public Shape getNewShape() {
+        return newShape;
+    }
+
+    public Shape getPrevShape() {
+        return prevShape;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public EditDrawCommand(Shape c, int index,Shape s){
+        prevShape = c;
+        newShape = s;
+        this.index = index;
+    }
+
+    public EditDrawCommand(){
+        prevShape = null;
+        index = 0;
+    }
+
+    public void editDrawCommand(double x, double y,String shape,int size, boolean fill,String color){
+        try {
+            int dc = findFirstOccurance(x, y);
+            Shape previousShape = ((DrawCommand) Command.getCommandHistory().get(dc)).getShape();
+            Shape tempShape = ShapeFactory.createShape(shape, previousShape.getX(), previousShape.getY(), size, size, fill, color);
+            ((DrawCommand) Command.getCommandHistory().get(dc)).setShape(tempShape);
+            //Command.getCommandHistory().add(0, new EditDrawCommand(ShapeFactory.createShape(shape, previousShape.getX(), previousShape.getY(), size, size, fill, color)));
+            Command.getCommandHistory().add(0, new EditDrawCommand(previousShape,index,tempShape));
+
+            setChanged();
+            notifyObservers(commandHistory);
+        }catch(IndexOutOfBoundsException e){
+            System.out.println("no object found");
+        }
+    }
 }
